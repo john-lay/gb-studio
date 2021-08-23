97346bfcc04c32ca35d6fe3e2999ebf65f4ff027
john-lay <john_lay@hotmail.co.uk>
2021-08-23 22:49:59 +0100

    Duplicate Top-Down state

---
+++ appData/src/gb/include/states/ZeldasAdventure.h
@@ -0,0 +1,9 @@
+#ifndef STATE_ZELDAS_ADVENTURE_H
+#define STATE_ZELDAS_ADVENTURE_H
+
+#include <gb/gb.h>
+
+void Start_ZeldasAdventure();
+void Update_ZeldasAdventure();
+
+#endif
--- appData/src/gb/src/main.c
+++ appData/src/gb/src/main.c
@@ -6,13 +6,14 @@
 #include "states/Shmup.h"
 #include "states/TopDown.h"
 #include "states/PointNClick.h"
+#include "states/ZeldasAdventure.h"
 #include <gb/bgb_emu.h>
 
 const Void_Func_Void startFuncs[] = {0, Start_TopDown, Start_Platform, Start_Adventure,
-                                     Start_Shmup, Start_PointNClick};
+                                     Start_Shmup, Start_PointNClick, Start_ZeldasAdventure};
 const Void_Func_Void updateFuncs[] = {0, Update_TopDown, Update_Platform, Update_Adventure,
-                                      Update_Shmup, Update_PointNClick};
-const UBYTE stateBanks[] = {0, 5, 5, 5, 5, 5};
+                                      Update_Shmup, Update_PointNClick, Update_ZeldasAdventure};
+const UBYTE stateBanks[] = {0, 5, 5, 5, 5, 5, 5};
 
 int main() {
     core_start();
